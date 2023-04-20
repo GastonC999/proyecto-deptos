cc9289e20e0bbda75b93f03c16ee0a6d5cd35178
Gaston Castro <gastonc08gec@gmail.com>
2023-04-19 23:03:22 -0300

    Creamos

--- src/main/java/com/gastoncastro/departamentos/modelo/departamento.java
+++ src/main/java/com/gastoncastro/departamentos/modelo/departamento.java
@@ -1,2 +1,58 @@
-package com.gastoncastro.departamentos.modelo;public class departamento {
+package com.gastoncastro.departamentos.modelo;
+
+public class departamento {
+
+    String nombre;
+    String direccion;
+    String numero;
+    String piso;
+
+    public departamento(String nombre, String direccion, String numero, String piso) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.numero = numero;
+        this.piso = piso;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getPiso() {
+        return piso;
+    }
+
+    public void setPiso(String piso) {
+        this.piso = piso;
+    }
+
+    @Override
+    public String toString() {
+        return "departamento{" +
+                "nombre='" + nombre + '\'' +
+                ", direccion='" + direccion + '\'' +
+                ", numero='" + numero + '\'' +
+                ", piso='" + piso + '\'' +
+                '}';
+    }
 }
