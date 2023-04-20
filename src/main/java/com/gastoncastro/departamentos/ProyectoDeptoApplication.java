f63da952d7f3e2c61db82e472f306e67c88a50b9
Gaston Castro <gastonc08gec@gmail.com>
2023-04-19 22:02:20 -0300

    primer commit

---
+++ src/main/java/com/gastoncastro/departamentos/ProyectoDeptoApplication.java
@@ -0,0 +1,13 @@
+package com.gastoncastro.departamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoDeptoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoDeptoApplication.class, args);
+	}
+
+}
