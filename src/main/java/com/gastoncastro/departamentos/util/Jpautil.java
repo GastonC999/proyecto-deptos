4a2f4ff106780f19e251de6dab34315c05099a4d
Shay <gastonc08gec@gmail.com>
2023-04-23 01:34:12 -0300

    Configuramos la clase Jpautil

--- src/main/java/com/gastoncastro/departamentos/util/Jpautil.java
+++ src/main/java/com/gastoncastro/departamentos/util/Jpautil.java
@@ -1,4 +1,16 @@
 package com.gastoncastro.departamentos.util;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Jpautil {
+    private static final EntityManagerFactory entityManagerFactory = buildEntityManagerFactory();
+    private static EntityManagerFactory buildEntityManagerFactory(){
+        return Persistence.createEntityManagerFactory("proyectoDeptos");
+    }
+    public static EntityManager getEntityManager(){
+        return entityManagerFactory.createEntityManager();
+    }
+
 }
