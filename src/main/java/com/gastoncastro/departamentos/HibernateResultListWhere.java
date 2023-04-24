720745067c7a1a66a6ccc594871c67de1467312e
Gaston Castro <gastonc08gec@gmail.com>
2023-04-24 09:40:15 -0300

    Utilizamos el where

---
+++ src/main/java/com/gastoncastro/departamentos/HibernateResultListWhere.java
@@ -0,0 +1,11 @@
+package com.gastoncastro.departamentos;
+
+import com.gastoncastro.departamentos.util.Jpautil;
+import jakarta.persistence.EntityManager;
+
+public class HibernateResultListWhere {
+    public static void main(String[] args) {
+        EntityManager em = Jpautil.getEntityManager();
+        
+    }
+}
