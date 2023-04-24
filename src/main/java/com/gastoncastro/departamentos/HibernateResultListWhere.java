8c2e3d4bbb8620c26f7a1f50d36d82eb880b097b
Gaston Castro <gastonc08gec@gmail.com>
2023-04-24 10:04:55 -0300

    probe usar el where, pero no anda del todo

--- src/main/java/com/gastoncastro/departamentos/HibernateResultListWhere.java
+++ src/main/java/com/gastoncastro/departamentos/HibernateResultListWhere.java
@@ -1,11 +1,16 @@
 package com.gastoncastro.departamentos;
 
+import com.gastoncastro.departamentos.modelo.Departamento;
 import com.gastoncastro.departamentos.util.Jpautil;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
 
 public class HibernateResultListWhere {
     public static void main(String[] args) {
         EntityManager em = Jpautil.getEntityManager();
-        
+        Query query = em.createQuery("select d from Departamento d where d.nombre=?1", Departamento.class);
+        query.setParameter(1, "Marisa");
+        Departamento d = (Departamento) query.getSingleResult();
+        em.close();
     }
 }
