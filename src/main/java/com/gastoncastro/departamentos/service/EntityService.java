3caf3baf280fa4271304efebf57bc11f8006cfc5
Shay <gastonc08gec@gmail.com>
2023-04-26 20:47:22 -0300

    Hice para que EntityService reciba Dtp y devuelva un Entity

--- src/main/java/com/gastoncastro/departamentos/service/EntityService.java
+++ src/main/java/com/gastoncastro/departamentos/service/EntityService.java
@@ -2,9 +2,9 @@ package com.gastoncastro.departamentos.service;
 
 import java.util.List;
 
-public interface EntityService<T> {
-    List<T> getListEntidades(String startWith);
-    T porId(Long id);
-    T guardar(T t);
+public interface EntityService<D, E> {
+    List<E> getListEntidades();
+    E porId(Long id);
+    E guardar(D dto);
     void eliminar(Long id);
 }
