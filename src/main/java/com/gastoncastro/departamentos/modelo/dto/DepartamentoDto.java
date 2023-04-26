acd5e4a4a74c5d96eb4d62acfc6deb2480adc25d
Shay <gastonc08gec@gmail.com>
2023-04-26 20:51:58 -0300

    Agregamos la clase dto

---
+++ src/main/java/com/gastoncastro/departamentos/modelo/dto/DepartamentoDto.java
@@ -0,0 +1,25 @@
+package com.gastoncastro.departamentos.modelo.dto;
+
+public class DepartamentoDto {
+
+    private String nombre;
+    private String direccion;
+    private String numero;
+    private String piso;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public String getPiso() {
+        return piso;
+    }
+}
