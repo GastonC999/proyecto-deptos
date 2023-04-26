056f3e228a066e40735b72d9ea8479d6f4103f3b
Shay <gastonc08gec@gmail.com>
2023-04-26 20:39:39 -0300

    acomodamos la firma del metodo con departamento y nos devuelve el departamentoDto

--- src/main/java/com/gastoncastro/departamentos/controller/DeptosControllers.java
+++ src/main/java/com/gastoncastro/departamentos/controller/DeptosControllers.java
@@ -1,8 +1,8 @@
 package com.gastoncastro.departamentos.controller;
 
-import com.gastoncastro.departamentos.modelo.Departamento;
+import com.gastoncastro.departamentos.modelo.entity.Departamento;
+import com.gastoncastro.departamentos.modelo.dto.DepartamentoDto;
 import com.gastoncastro.departamentos.service.DeptosService;
-import jakarta.ws.rs.QueryParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,28 +17,30 @@ public class DeptosControllers {
     @Autowired
     private DeptosService deptosService;
 
-    @PostMapping
-    public ResponseEntity<Departamento> guardarCandidato (@RequestBody Departamento departamento){
-        return new ResponseEntity<Departamento>(deptosService.guardar(departamento),
+    @PostMapping("/agregar")
+    //firma de metodo
+    public ResponseEntity<Departamento> guardarDepartamento (@RequestBody DepartamentoDto departamentoDto){
+        return new ResponseEntity<Departamento>(deptosService.guardar(departamentoDto),
         HttpStatus.CREATED);
     }
+
     @GetMapping("/{id}")
     public ResponseEntity<Departamento> getDeptoById(@PathVariable("id") long id){
         return new ResponseEntity<Departamento>(deptosService.porId(id), HttpStatus.OK);
     }
     @GetMapping
-    public ResponseEntity<List<Departamento>> getDeptos(@QueryParam("departamentos") String departamento){
-        return new ResponseEntity<List<Departamento>> (deptosService.getListEntidades(departamento), HttpStatus.OK);
+    public ResponseEntity<List<Departamento>> getDeptos(){
+        return new ResponseEntity<List<Departamento>> (deptosService.getListEntidades(), HttpStatus.OK);
     }
 
     @PutMapping(value= "/{id}")
     public ResponseEntity<Departamento> updateUser(@PathVariable("id") long id, @RequestBody Departamento departamento){
-        return new ResponseEntity<Departamento>(deptosService.UpdateDepto(departamento, departamento.getId()), HttpStatus.OK);
+        return new ResponseEntity<Departamento>(deptosService.updateDepto(departamento, departamento.getId()), HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteDepto(@PathVariable("id") long id){
-        deptosService.deleteDepto(id);
+        deptosService.eliminar(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
