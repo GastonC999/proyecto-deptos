c406b602aa414d452caf14710509d516aa834fd0
Gaston Castro <gcastro@mobydigital.com>
2023-05-06 20:21:51 -0300

    Create Persona class

---
+++ src/main/java/com/gastoncastro/departamentos/modelo/entity/Persona.java
@@ -0,0 +1,11 @@
+package com.gastoncastro.departamentos.modelo.entity;
+
+public class Persona {
+
+    private String nombre;
+    private String apellido;
+    private String fechaNacimiento;
+    private 
+    private int dni;
+
+}
