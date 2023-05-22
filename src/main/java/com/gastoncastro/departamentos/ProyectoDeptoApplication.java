6f4e6bb6fce28391bba57ec9626ba244b9e72234
Gaston Castro <gcastro@mobydigital.com>
2023-05-22 15:07:50 -0300

    comentamos el el sistem

--- src/main/java/com/gastoncastro/departamentos/ProyectoDeptoApplication.java
+++ src/main/java/com/gastoncastro/departamentos/ProyectoDeptoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProyectoDeptoApplication {
 	public static void main(String[] args) {
-		System.setProperty("spring.devtools.restart.enabled", "false");
+		// System.setProperty("spring.devtools.restart.enabled", "false");
 		SpringApplication.run(ProyectoDeptoApplication.class, args);
 	}
 
